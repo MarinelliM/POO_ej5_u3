1bc12c1167b13e1496191389254de7456a993212
MarinelliM <103908022+MarinelliM@users.noreply.github.com>
2022-06-02 22:28:58 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from zope.interface import Interface
+
+
+class Intefaz(Interface):
+
+    def insertarElemento(self, elemento, pos):
+        pass
+
+    def agregarElemento(self, elemento):
+        pass
+
+    def mostrarElemento(self):
+        pass
